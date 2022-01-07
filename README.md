d988076a0f2acde4b2d6367fece99a4d2dc7cf66
StevenVallarsa <orionsjv@gmail.com>
2022-01-07 17:42:15 -0500

    completed basic functionality

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 - A simple program that will calculate what breeds make up your dog.
 - Creates a random profile of your dog's makeup.
 
+### How It Works
+- Asks for your furbaby's name
+- makes an ArrayList of up to five numbers that add up to 100
+- orders list in descending order
+- prints percentages along with a randomly selected dog breed
+- tells you your dog is weird :)
+
 
 ### Stack
 - Java 11
--- src/main/java/com/sv/doggenetics2022/DogGenetics.java
+++ src/main/java/com/sv/doggenetics2022/DogGenetics.java
@@ -5,6 +5,12 @@
 
 package com.sv.doggenetics2022;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author: Steven Vallarsa
@@ -14,4 +20,84 @@ package com.sv.doggenetics2022;
  */
 public class DogGenetics {
 
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        
+        int percentageLeft = 100;
+        int breedsLeft = 5;
+        
+        List<Integer> breedPercentages = new ArrayList<>();
+        
+        String[] dogBreeds = {
+            "Irish Wolfhound",
+            "Xoloitzcuintli",
+            "Australian Stumpy Tail Cattle Dog",
+            "Pembroke Welsh Corgi",
+            "Cardigan Corgi",
+            "Alaskan Klee Kai",
+            "Brussels Griffon",
+            "Catalburun",
+            "Bergamasco Shepherd",
+            "Lowchen",
+            "Puli",
+            "Russian Borzoi",
+            "Affenpinscher",
+            "Neapolitan Mastiff"
+        };
+        
+        System.out.println("WELCOME TO DOG GENETICS");
+        System.out.println("-----------------------\n");
+        System.out.println("What's your furbaby's name?");
+        
+        String name = scanner.nextLine();
+        
+        System.out.println("\nGenerating results...\n");
+        
+        System.out.printf("Oh my! %s is a mix weird mix of:%n", name);
+        
+        
+        // Create max 5 percentages in List
+        while (percentageLeft > 0) {
+            
+            int percentage;
+            
+            if (breedsLeft == 1) {
+                percentage = percentageLeft;
+            } else {
+                percentage = random.nextInt(percentageLeft) + 1;
+            }
+            
+            breedPercentages.add(percentage);
+            percentageLeft -= percentage;
+            
+            breedsLeft--;
+        }
+        
+        
+        // Order list in descending order
+        Collections.sort(breedPercentages, Collections.reverseOrder());
+
+        
+        // Display results
+        for (int percentage : breedPercentages) {
+            
+            boolean notRepeadBreed = true;
+            
+            while (notRepeadBreed) {
+                
+                int index = random.nextInt(dogBreeds.length);
+
+                if (dogBreeds[index] != "") {
+                    System.out.printf(" %2d%% %s%n" , percentage, dogBreeds[index]);
+                    dogBreeds[index] = "";
+                    notRepeadBreed = false;
+                } 
+            }
+        }
+        
+        System.out.println("\nThat's one weird dog!");
+    }
+    
 }
